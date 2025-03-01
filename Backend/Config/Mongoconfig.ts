6cde571e6ac662e04689c2b9ce628fbc02a49cd6
AbhiWorkswithFlutter <tiabhi1999@gmail.com>
2025-03-02 01:31:04 +0530

    Initial

--- Backend/Config/Mongoconfig.ts
+++ Backend/Config/Mongoconfig.ts
@@ -1,4 +1,6 @@
 import { MongoClient, Db } from 'mongodb';
+import * as dotenv from 'dotenv';
+dotenv.config(); 
 
 export class MongoConfig {
     private static dbConnection: Db | null = null;
@@ -10,8 +12,8 @@ export class MongoConfig {
         }
 //process.env.COSMOS_DB_CONNECTION_STRING ||
 //process.env.COSMOS_DB_NAME ||
-        const cosmosDbConnectionString =  `mongodb://guessgame:Assignment%40123@cluster0-shard-00-00.lg29j.mongodb.net:27017,cluster0-shard-00-01.lg29j.mongodb.net:27017,cluster0-shard-00-02.lg29j.mongodb.net:27017/?ssl=true&replicaSet=atlas-132az8-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`;
-        const cosmosDbName =  "guessgame";
+        const cosmosDbConnectionString = process.env.DB_CONNECTION_STRING ||  `mongodb://guessgame:Assignment%40123@cluster0-shard-00-00.lg29j.mongodb.net:27017,cluster0-shard-00-01.lg29j.mongodb.net:27017,cluster0-shard-00-02.lg29j.mongodb.net:27017/?ssl=true&replicaSet=atlas-132az8-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0`;
+        const cosmosDbName = process.env.DB_NAME ||  "guessgame";
 
         if (!cosmosDbConnectionString || !cosmosDbName) {
             throw new Error("Database connection string or name is missing.");
--- Backend/index.ts
+++ Backend/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+
 // Importing routers
 import { GameRoutes } from './Routes/GameRoutes';
 import { userRoutes } from './Routes/UserRoutes';
+import * as dotenv from 'dotenv';
+dotenv.config(); 
 
 
 
@@ -12,27 +15,25 @@ const user = new userRoutes();
   
 
 
-//Importing admin routers
-
 
 // Initialize Express app
 const app = express();
 
-// Connect to database
+
 
 // Middleware
 app.use(cors());
 app.use(express.json({ extended: false }));
 
-console.log("here1")
+
 // Routes
 app.use("/api/games", game.router);
 app.use("/api/players", user.router);
 
 
 
-// Start the server process.env.PORT ||
-const PORT =  8080;
+// Start the server 
+const PORT =  process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log("server started", PORT);
 });
